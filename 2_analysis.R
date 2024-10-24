883f8c1c58982f86e9ea9bbe4487e124ba90a3dd
danielcapistrano <39242880+danielcapistrano@users.noreply.github.com>
2024-10-24 18:48:25 -0400

    change ols plot

--- 2_analysis.R
+++ 2_analysis.R
@@ -6,7 +6,7 @@
 library(tidyverse) # for data manipulation
 library(haven) # to import/export from/to SPSS/STATA formats
 library(gt) # formatted tables
-library(sjPlot) # plot regression coefficients
+library(cowplot) # arrange multiple plots
 
 # Importing working dataset
 # Please see 1_process.R for the data processing that generated this file
@@ -28,26 +28,59 @@ gtsave(tb_sample, "./img/tb_sample.png")
 ###############################################################################
 # Generate Figure 2.1: OLS of congruence rates by ethnic fr  ########################
 
-get_plot <- function(target_variable, plot_title){
+get_plot <- function(resp_var, title = "", rm_countries = NULL){
+  estimates <- 
+    df |>
+      filter(!country_survey %in% rm_countries) |>
+      mutate(cntry = if_else(region_es == "Majority", "_Majority", country_name)) |>
+      group_by(country_survey) |>
+      group_modify(~ broom::tidy(lm(get(resp_var) ~ cntry, weights = Weging, data = .x), conf.int = TRUE)) |>
+      mutate(term = str_remove(term, "cntry")) |>
+      mutate(region_cntry = case_match(term,
+        c("Morocco", "Pakistan", "Turkey") ~ "MENAP",
+        c("Congo", "Nigeria", "Senegal") ~ "SSA",
+        .default = "Other"))
 
-  model <- lm(df[[target_variable]] ~ region_es, weights = Weging, data = df)
-  plot <- sjPlot:::plot_model(
-    model,
-    #terms = "region_es [SSA, MENAP, Other]",
-    #axis.labels = c("SSA", "MENAP", "Other"),
-    title = plot_title
-    ) +
+  estimates$region_cntry <- factor(estimates$region_cntry, levels=c("MENAP", "SSA", "Other"))
+  
+  estimates |>
+    filter(term != "(Intercept)")  |>
+    ggplot(aes(x = term, y = estimate, ymin = conf.low, ymax = conf.high, color = country_survey)) +
+    geom_hline(yintercept = 0, linetype='dashed') +
+    geom_pointrange() +
+    scale_color_brewer(palette = "Set3") +
+    coord_flip() +
+    #ylim(-0.6, 0.2) +
     theme_classic() +
-    geom_hline(yintercept=0, linetype='dashed') +
-    ylim(-0.6, 0.6)
-
-  return(plot)
+    theme(plot.background = element_blank()) +
+    labs(title = title, y = NULL, x = NULL) +
+    facet_wrap(~region_cntry, ncol = 1, strip.position = 'left', scales = "free_y")
 }
 
-fig_vars <- c("cong_religion", "cong_sex", "cong_region", "skin_adj")
-fig_titles <- c(paste0("Congruence rate for ", c("religion", "sex", "region")), "Monk skin color")
+plot_sex <- get_plot("cong_sex", title = "Sex")
+plot_ses <- get_plot("ladder_adj", title = "SES")
+plot_skin <- get_plot("skin_adj", title = "Skin" , rm_countries = c("Czech Republic", "Hungary"))
+plot_relig <- get_plot("cong_religion", title = "Religion", rm_countries = c("Czech Republic", "Hungary"))
 
-fig_ols <- plot_grid(map2(fig_vars, fig_titles, ~get_plot(.x, .y)), tags = rep("", 4))
+legend <- get_plot_component(
+  plot_ses +  
+  guides(color=guide_legend(nrow=1,byrow=TRUE)) +
+  theme(legend.position = "bottom")+
+  theme(legend.title=element_blank()), 
+  "guide-box-bottom",
+  return_all = TRUE
+)
+
+grid <- 
+  plot_grid(
+    plot_sex + theme(legend.position = "none"),
+    plot_ses + theme(legend.position = "none"), 
+    plot_skin + theme(legend.position = "none"), 
+    plot_relig + theme(legend.position = "none"), 
+    ncol = 2
+  )
+
+fig_ols  <- plot_grid(grid, legend, ncol = 1, rel_heights = c(3, .4))
 
 ggsave(fig_ols, filename =  "./img/fig_ols.png")
 
